dabe8bfc642fb193d039e4942e00ab17148165e0
goranikin <goranikin@snu.ac.kr>
2025-04-03 10:01:10 +0900

    🎨 Dd

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -8,51 +8,51 @@ import { useRouter } from "next/navigation";
 import MobileNav from "./mobile-nav";
 
 export const Header = () => {
-  return (
-    <header className="h-14 px-5 sticky top-0 z-50 w-full border-b flex justify-evenly items-center bg-background gap-5">
-      <div className="flex items-center gap-5">
-        <HomeLogo />
-        <BackButton />
-      </div>
-      <div className="flex grow items-center">
-        <div className="hidden sm:flex flex-1 justify-center">
-          <Nav />
-        </div>
-        <div className="ml-auto flex items-center sm:hidden">
-          <MobileNav />
-        </div>
-      </div>
-    </header>
-  );
+	return (
+		<header className="h-14 px-5 sticky top-0 z-50 w-full border-b flex justify-evenly items-center bg-background gap-5">
+			<div className="flex items-center gap-5">
+				<HomeLogo />
+				<BackButton />
+			</div>
+			<div className="flex grow items-center">
+				<div className="hidden sm:flex flex-1 justify-center">
+					<Nav />
+				</div>
+				<div className="ml-auto flex items-center sm:hidden">
+					<MobileNav />
+				</div>
+			</div>
+		</header>
+	);
 };
 
 const HomeLogo = () => {
-  return (
-    <Link href="/">
-      <Button
-        type="button"
-        variant="outline"
-        size="icon"
-        className="rounded-full"
-      >
-        <Home className="h-5 w-5 transition-all text-primary stroke-1" />
-      </Button>
-    </Link>
-  );
+	return (
+		<Link href="/">
+			<Button
+				type="button"
+				variant="outline"
+				size="icon"
+				className="rounded-full"
+			>
+				<Home className="h-5 w-5 transition-all text-primary stroke-1" />
+			</Button>
+		</Link>
+	);
 };
 
 const BackButton = () => {
-  const router = useRouter(); // 라우터 인스턴스 생성
+	const router = useRouter(); // 라우터 인스턴스 생성
 
-  return (
-    <Button
-      type="button"
-      variant="outline"
-      size="icon"
-      className="rounded-full"
-      onClick={() => router.back()} // 뒤로가기 기능 추가
-    >
-      <ArrowLeft className="h-5 w-5 transition-all text-primary stroke-1" />
-    </Button>
-  );
+	return (
+		<Button
+			type="button"
+			variant="outline"
+			size="icon"
+			className="rounded-full"
+			onClick={() => router.back()} // 뒤로가기 기능 추가
+		>
+			<ArrowLeft className="h-5 w-5 transition-all text-primary stroke-1" />
+		</Button>
+	);
 };
--- src/components/postPageLayout.tsx
+++ src/components/postPageLayout.tsx
@@ -2,20 +2,20 @@ import { MDXContent } from "@/mdx-components";
 import type { Post } from "@/utils/post";
 
 type Props = {
-  post: Post;
+	post: Post;
 };
 
 const PostPageLayout = (props: Props) => {
-  return (
-    <div className="flex flex-col mt-5 gap-10">
-      <h1 className="text-4xl font-bold whitespace-pre-wrap">
-        {props.post.title}
-      </h1>
-      <div className="prose max-w-full">
-        <MDXContent code={props.post.content} />
-      </div>
-    </div>
-  );
+	return (
+		<div className="flex flex-col mt-5 gap-10">
+			<h1 className="text-4xl font-bold whitespace-pre-wrap">
+				{props.post.title}
+			</h1>
+			<div className="prose max-w-full">
+				<MDXContent code={props.post.content} />
+			</div>
+		</div>
+	);
 };
 
 export default PostPageLayout;
