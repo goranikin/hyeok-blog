8fa0d5295876afa9caf7e073e263084a7f7f5a7f
goranikin <goranikin@snu.ac.kr>
2025-04-09 19:42:23 +0900

    🎨 Update 2

--- src/components/postList.tsx
+++ src/components/postList.tsx
@@ -3,53 +3,55 @@ import Image from "next/image";
 import Link from "next/link";
 
 interface PostListProps {
-	posts: Post[];
-	basePath: string; // 이동할 기본 경로 (예: '/development', '/writing/book-review')
+  posts: Post[];
+  basePath: string; // 이동할 기본 경로 (예: '/development', '/writing/book-review')
 }
 
 const DEFAULT_IMAGE = "/itisme.png";
 
 export default function PostList({ posts, basePath }: PostListProps) {
-	return (
-		<div>
-			{posts
-				.sort(
-					(a, b) =>
-						Number(new Date(b.publishDate)) - Number(new Date(a.publishDate)),
-				)
-				.map((post: Post) => {
-					const date = new Date(post.publishDate);
-					const year = date.getFullYear();
-					const month = date.getMonth() + 1;
-					const day = date.getDate();
+  return (
+    <div>
+      {posts
+        .sort(
+          (a, b) =>
+            Number(new Date(b.publishDate)) - Number(new Date(a.publishDate)),
+        )
+        .map((post: Post) => {
+          const date = new Date(post.publishDate);
+          const year = date.getFullYear();
+          const month = date.getMonth() + 1;
+          const day = date.getDate();
 
-					return (
-						<Link
-							href={`${basePath}/${post.slug}`}
-							key={post.slug}
-							className="flex py-5 items-center justify-between gap-2 border-b last:border-none"
-						>
-							<div className="flex flex-col gap-1 flex-1">
-								<span className="font-semibold text-lg break-all line-clamp-2">
-									{post.title}
-								</span>
-								<span className="break-all">
-									{basePath.includes("writing") ? "" : post.description}
-								</span>
-								<time className="text-xs mt-1">
-									{year}.{month}.{day}
-								</time>
-							</div>
-							<Image
-								width={200}
-								height={200}
-								src={post.thumbnailUrl ?? DEFAULT_IMAGE}
-								alt={post.title}
-								className="object-cover w-28 h-28"
-							/>
-						</Link>
-					);
-				})}
-		</div>
-	);
+          return (
+            <Link
+              href={`${basePath}/${post.slug}`}
+              key={post.slug}
+              className="flex py-5 items-center justify-between gap-2 border-b last:border-none"
+            >
+              <div className="flex flex-col gap-1 flex-1">
+                <span className="font-semibold text-lg break-all line-clamp-2">
+                  {post.title}
+                </span>
+                <span className="break-all">
+                  {basePath.includes("writing") ? "" : post.description}
+                </span>
+                <time className="text-xs mt-1">
+                  {year}.{month}.{day}
+                </time>
+              </div>
+              {post.thumbnailUrl ? (
+                <Image
+                  width={200}
+                  height={200}
+                  src={post.thumbnailUrl ?? DEFAULT_IMAGE}
+                  alt={post.title}
+                  className="object-cover w-28 h-28"
+                />
+              ) : null}
+            </Link>
+          );
+        })}
+    </div>
+  );
 }
