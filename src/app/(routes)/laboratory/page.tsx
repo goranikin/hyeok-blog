46413d812a08820b8e5317112fc2b5e1abd8e084
goranikin <goranikin@snu.ac.kr>
2025-05-28 19:43:34 +0900

    🎨 방명록 -> 실험실 & 이력 삭

---
+++ src/app/(routes)/laboratory/page.tsx
@@ -0,0 +1,9 @@
+import PageLayout from "@/components/pageLayout";
+
+export default async function GuestbookPage() {
+  return (
+    <PageLayout title="실험실" description="이것 저것 만들어보는 곳">
+      <div> </div>
+    </PageLayout>
+  );
+}
